53518e431c07f6a1a44d2f1ba8ffe1c1a5b7c1c8
Brian Acosta <brian.acosta@xing.com>
2020-10-21 22:55:04 +0200

    Added Google styling to components and a theme

---
+++ traininglog/src/config/theme.config.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from 'react';
+import { withFirebase } from '../components/Firebase';
+import { Link, withRouter } from 'react-router-dom';
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+
+import useStyles from '../config/theme-signinup';
+import Copyright from '../components/Copyright';
+
+function SignUp(props) {
+  const classes = useStyles();
+
+  const initialUser = {id: null, name: '', email: '', password: '', error: null, auth: null}
+
+  const [user, setUser] = useState(initialUser);
+
+  const handleChange = e => {
+    const {name, value} = e.target;
+    setUser({...user, [name]: value})
+  }
+
+  const handleSubmit = e => {
+    props.firebase.auth.createUserWithEmailAndPassword(user.email, user.password)
+    // Later add user also to database
+    .then(authUser => {
+      setUser(initialUser);
+      props.history.push("/dashboard");
+    })
+    .catch(error => {
+      setUser({...user, error: error.message})
+    });
+  }
+
+  const isValid = user.name === '' || user.email === '' || user.password === '';
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign Up
+          </Typography>
+          <form 
+            className={classes.form} 
+            noValidate
+            onSubmit={e => e.preventDefault()}
+          >
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="name"
+              label="Name"
+              name="name"
+              autoFocus
+              value={user.name}
+              onChange={handleChange}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={handleChange}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handleChange}
+            />
+            <Typography className={classes.error}>
+              {user.error ? user.error : ''}
+            </Typography>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={handleSubmit}
+              disabled={isValid}
+            >
+              Sign up
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link to="/">
+                  {"Already have an account? Sign In"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default withRouter(withFirebase(SignUp));
