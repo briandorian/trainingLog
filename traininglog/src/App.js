d56df9f6de4eb80e7380e70f3b472bd116d18d09
Brian Acosta <brian.acosta@xing.com>
2020-10-21 22:39:27 +0200

    Created folders component, config and pages.Installed react router dom and added a route to signup and signin in the app.js file

--- traininglog/src/App.js
+++ traininglog/src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
-   return "hola";
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          Signin
+        </Route>
+        <Route path="/sign-up">
+          Signup
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
