c8bdf9fecad3522c6fde9ad257a6a81bd89f52de
Brian Acosta <brian.acosta@xing.com>
2020-10-22 08:39:05 +0200

    modified themes and created copyright component

---
+++ traininglog/src/components/Copyright/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      {'Created by Brian Acosta '}
+        {new Date().getFullYear()}
+        {'.'}
+    </Typography>
+  );
+}
+
+export default Copyright;
--- traininglog/src/pages/SignIn.jsx
+++ traininglog/src/pages/SignIn.jsx
@@ -12,19 +12,9 @@ import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import Copyright from '.././components/Copyright/index'
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
